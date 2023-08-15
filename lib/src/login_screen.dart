659b2651b6b510e88929f2f61bf8a964958cac84
JuYeon Kim <z01_k@naver.com>
2023-08-15 14:56:39 +0900

    구글 로그인 성공했는데 로그인 후 다음 연결 화면을 구현 못함

--- lib/src/login_screen.dart
+++ lib/src/login_screen.dart
@@ -1,4 +1,121 @@
 import 'package:flutter/material.dart';
+import 'package:walking_record/src/login_auth_service.dart';
+
+/*class LoginPage extends StatefulWidget {
+  const LoginPage({super.key});
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[300],
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.fromLTRB(30.0, 40.0, 30.0, 0.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const SizedBox(
+                height: 450,
+              ),
+              Text(
+                '하루의 산책',
+                style: TextStyle(
+                  color: Colors.grey[700],
+                  fontSize: 40,
+                ),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              Text(
+                '당신의 산책을',
+                style: TextStyle(
+                  color: Colors.grey[700],
+                  fontSize: 20,
+                ),
+              ),
+              Text(
+                '하루의 산책에 기록해보세요!',
+                style: TextStyle(
+                  color: Colors.grey[700],
+                  fontSize: 20,
+                ),
+              ),
+              const SizedBox(
+                height: 25.0,
+              ),
+
+              // 소셜로그인 버튼(구글, 카카오, 네이버)
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  IconButton(
+                    onPressed: () {
+                      print('카톡로그인');
+                    },
+                    icon: const CircleAvatar(
+                      radius: 30.0,
+                      backgroundColor: Color(0xffFEE500),
+                      child: CircleAvatar(
+                        radius: 21.0,
+                        backgroundImage: AssetImage('assets/kakao.png'),
+                      ),
+                    ),
+                  ),
+                  IconButton(
+                    onPressed: () {
+                      print('네이버로그인');
+                    },
+                    icon: const CircleAvatar(
+                      radius: 30.0,
+                      backgroundImage: AssetImage('assets/naver.png'),
+                    ),
+                  ),
+                  IconButton(
+                    onPressed: () async =>
+                        await AuthService().signInWithGoogle(),
+
+                    // onPressed: () {
+                    //   print('구글로그인');
+                    // },
+                    icon: const CircleAvatar(
+                      radius: 30.0,
+                      backgroundColor: Colors.white,
+                      child: CircleAvatar(
+                        radius: 18.0,
+                        backgroundImage: AssetImage('assets/google.png'),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              FutureBuilder(
+                future: Authentication.initializeFirebase(/*context:context*/),
+                builder: (context, snapshot) {
+                  if (snapshot.hasError) {
+                    return Text('Error initializing Firebase');
+                  } else if (snapshot.connectionState == ConnectionState.done) {
+                    return GoogleSignInButton();
+                  }
+                  return CircularProgressIndicator(
+                    valueColor: AlwaysStoppedAnimation<Color>(
+                      Colors.orange,
+                    ),
+                  );
+                },
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}*/
 
 class LoginPage extends StatelessWidget {
   const LoginPage({super.key});
@@ -71,9 +188,12 @@ class LoginPage extends StatelessWidget {
                     ),
                   ),
                   IconButton(
-                    onPressed: () {
-                      print('구글로그인');
-                    },
+                    onPressed: () async =>
+                        await AuthService().signInWithGoogle(),
+
+                    // onPressed: () {
+                    //   print('구글로그인');
+                    // },
                     icon: const CircleAvatar(
                       radius: 30.0,
                       backgroundColor: Colors.white,
